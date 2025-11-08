797aead437101e0d04a2db035250b04397cf43dd
ThomasGus <tgus@uw.edu>
2025-11-08 15:54:10 -0800

    add post position file and to the app file

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import ReactDOM from 'react-dom/client';
 import { Header } from './Header.jsx';
 import HomePageBody from './HomePageBody.jsx'
 import EmptyPost from './empty-post.jsx';
+import PostPosition from './post-position.jsx';
 
 export default function App(props) {
     return (
         <div>
             <Header />
             <HomePageBody />
+            <PostPosition />
+            <EmptyPost />
         </div>
     )
 }
