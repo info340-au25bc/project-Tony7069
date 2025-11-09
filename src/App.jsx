75b11084fe4d5b4ad22db255c834e657a59b7c6a
Xinyue Li <tony.li.xinyue@outlook.com>
2025-11-08 16:24:50 -0800

    empty post & post position page included

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,16 @@ import ReactDOM from 'react-dom/client';
 import { Routes, Route } from "react-router-dom";
 
 import Homepage from './Homepage.jsx';
-// import EmptyPost from './empty-post.jsx';
-// import PostPosition from './post-position.jsx';
+import EmptyPost from './empty-post.jsx';
+import PostPosition from './post-position.jsx';
 
 export default function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        {/* <Route path="/create-post" element={<EmptyPost />} /> */}
-        {/* <Route path="/position" element={<PostPosition />} /> */}
+        <Route path="/create-post" element={<EmptyPost />} />
+        <Route path="/position" element={<PostPosition />} />
       </Routes>
     </>
   );
