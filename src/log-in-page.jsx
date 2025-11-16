47afdca0bad7012a0b669e101e0fa2f7826425bd
Xinyue Li <tony.li.xinyue@outlook.com>
2025-11-16 10:14:40 -0800

    login page background slightly adjusted

--- src/log-in-page.jsx
+++ src/log-in-page.jsx
@@ -1,6 +1,13 @@
 export default function LoginPage() {
   return (
     <div className="login-signup">
+      {/* This is for fun */}
+      {/* <div className='video-background'>
+        <video autoPlay muted loop playsInline>
+          <source src="/DiscoveryPark.mp4" type="video/mp4" />
+        </video>
+      </div> */}
+
       <div className="input-container">
         <div className="input-container-content">
           <h2>Internshipper</h2>
