565ac76189826dd258bd5e357861a71c331b2fe2
Tony7069 <tony.li.xinyue@outlook.com>
2025-11-20 18:55:04 -0800

    hosted on firebase

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,27 @@ import App from './App.jsx';
 
 import { BrowserRouter } from "react-router-dom";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCX6MQyB8IwWpB4iJLJF7oIHf6Sb9lOAGA",
+  authDomain: "internshipper-44a1c.firebaseapp.com",
+  projectId: "internshipper-44a1c",
+  storageBucket: "internshipper-44a1c.firebasestorage.app",
+  messagingSenderId: "949845215124",
+  appId: "1:949845215124:web:6f333d25499454ef0ef181",
+  measurementId: "G-44Y7YWF2J0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
